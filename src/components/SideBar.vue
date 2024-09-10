3f4f4b0f30e1493c48e49e81cd776a4f08c2313b
CatherineFD <firsov4.catherine@yandex.ru>
2024-09-10 12:42:46 +0300

    initial commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<script>
+import SearchInput from "@/components/SideBar/SearchInput.vue";
+import ResultsSearch from "@/components/SideBar/ResultsSearch.vue";
+export default {
+  name: "SideBar",
+  components: {
+    SearchInput,
+    ResultsSearch
+  }
+}
+</script>
+
+<template>
+  <div>
+    <SearchInput></SearchInput>
+    <ResultsSearch></ResultsSearch>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,41 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import {getItem, setItem} from "@/store/localStorage";
+import api from '@/api';
 
 export default createStore({
   state: {
+    users: getItem('users') || []
+
   },
   getters: {
+    getUsers(state) {
+      return state.users;
+    }
   },
   mutations: {
+    setUsers(state, users) {
+      state.users = users;
+      setItem('users', users)
+    },
+    clearUsers(state) {
+      localStorage.clear();
+    }
   },
   actions: {
+    getUsersByUsername({commit}, search) {
+      let users = [];
+      let searchItem = search.split(', ');
+      if (searchItem.length > 1) {
+        api.getUsersByUserNames(searchItem)
+            .then(res => {
+              commit('setUsers', res.data)});
+      } else {
+        api.getUserByUserName(searchItem[0]).then(res => {
+          commit('setUsers', res.data)});
+      }
+
+      commit('setUsers', users);
+    }
   },
   modules: {
   }
